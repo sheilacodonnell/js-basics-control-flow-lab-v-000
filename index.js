e04240c57eee50608719105db9d1450ecbeee125
Sheila O'Donnell <sheila@contexttravel.com>
2018-08-11 17:37:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(someValue) {
     result = 'This one is on me!'
   }
   
-  else if (someValue > 2000) {
+  else if (someValue > 2000 && < 2500) {
     result = 'I will gladly take your thirty bucks.'
   }
   
